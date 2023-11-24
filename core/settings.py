8e6da8d172fa9a24cce9a4844f005624bc691168
alok-kumar8765 <>
2023-11-24 13:51:20 +0000

    added requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o_t82$d45=zu8vza(gdy&@u1v^&p!fjn#^s7%=9qlj+dwz-_z-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
